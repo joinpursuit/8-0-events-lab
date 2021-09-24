531b79b7f17ed88ce447fbd04340f218f0ccd35f
Eva Rodriguez <evarodriguez@Evas-MacBook-Pro.local>
2021-09-24 14:54:43 -0400

    Finished lab

--- main.js
+++ main.js
@@ -7,3 +7,16 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+const selectPalette = document.querySelector('#palette div').style.background
+document.querySelectorAll("#palette div").forEach(div => {
+  div.addEventListener('click', selectColor => {
+    document.querySelector('#current-color').style.background = selectColor.target.style.background;
+  })
+});
+
+document.querySelectorAll('.cell').forEach(cell => {
+  cell.addEventListener('click', selectedCell => {
+    selectedCell.target.style.background = document.querySelector('#current-color').style.background;
+  })
+});
