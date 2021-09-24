b26369832acbe39793db8bc38090c6d01e143d3b
BennyZ3 <bennyz3@hotmail.com>
2021-09-24 14:30:57 -0400

    added variable number of squares, fill/reset buttons

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 // Do not change the code below.
 const main = document.querySelector("main");
-for (let i = 0; i < 100; i++) {
+let size = 25;
+for (let i = 0; i < size * size; i++) {
   const div = document.createElement("div");
   div.classList.add("cell");
   main.append(div);
 }
-
+document.querySelector(
+  "#canvas"
+).style.gridTemplateColumns = `repeat(${size}, ${500 / size}px)`;
+document.querySelector("#canvas").style.gridTemplateRows = `repeat(${size}, ${
+  500 / size
+}px)`;
 // You may write your code here!
 const currentColor = document.querySelector("#current-color");
 const colors = document.querySelectorAll(".color");
@@ -21,3 +27,35 @@ cells.forEach((cell) => {
     cell.style.background = `${currentColor.style.background}`;
   });
 });
+//resetButton
+const resetButton = document.createElement("button");
+resetButton.innerHTML = "reset";
+resetButton.addEventListener("click", (event) => {
+  cells.forEach((cell) => {
+    cell.style.background = "white";
+  });
+});
+document.querySelector("#palette").append(resetButton);
+
+//fillButton
+const fillButton = document.createElement("button");
+fillButton.innerHTML = "Fill";
+fillButton.addEventListener("click", (event) => {
+  cells.forEach((cell) => {
+    cell.style.background = `${currentColor.style.background}`;
+  });
+});
+document.querySelector("#palette").append(fillButton);
+
+//mouse move test
+// function mouseMoveWhileDown(target, whileMove) {
+//   const endMove = function () {
+//     window.removeEventListener("mousemove", whileMove);
+//     window.removeEventListener("mouseup", endMove);
+//   };
+//   target.addEventListener("mousedown", function (event) {
+//     event.stopPropagation(); // remove if you do want it to propagate ..
+//     window.addEventListener("mousemove", whileMove);
+//     window.addEventListener("mouseup", endMove);
+//   });
+// }
