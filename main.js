5767de0ad4ae632e8fac43e645adef9eb9ba4643
Rose Durandisse <rosedurandisse@pursuit.org>
2021-09-24 14:52:41 -0400

    Complete Main JS

--- main.js
+++ main.js
@@ -7,3 +7,32 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+//declare the paleete 
+const palette = document.querySelectorAll("#palette");
+//loop through the colors of the cell
+palette.forEach((eachColor) => {
+  //add an event when a cell is selected
+  eachColor.addEventListener('click', (event) => {
+    console.log(event);
+    //change the color of the current color when a palette color is selected
+    document.querySelector('#current-color').style.background = event.target.style.background
+  })
+})
+
+//add a click listener to the cell when it's clicked
+//tranform the cell color to  the color of the color stored in the color
+//but it can't change all the cells that are already colored
+
+const cell = document.querySelectorAll(`.cell`);
+cell.forEach((eachCell) => {
+  eachCell.addEventListener('click', (event) => {
+    event.target.style.background =
+      document.querySelector('#current-color').style.background
+  })
+})
+
+// const canvas = document.querySelector("#canvas");
+// canvas.addEventListener("click", (event) => {
+//   event.target.style.background = document.querySelector("#current-color").style.background;
+// });
