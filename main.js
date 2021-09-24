ad8951dc82ea9fd3794bec5c140c07de6e214050
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2021-09-24 18:35:24 -0400

    Fun Pixel Art Game using Event listeners and the dom

--- main.js
+++ main.js
@@ -7,3 +7,19 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+    const currentColors = document.querySelector("#current-color");
+    const initColors = document.querySelectorAll(".color");
+    const cells = document.querySelectorAll(".cell");
+
+    for(let colors of initColors){
+      colors.addEventListener("click", (event) =>{
+        currentColors.style.backgroundColor = event.target.style.backgroundColor;
+    });
+};
+
+    for(let cell of cells){
+      cell.addEventListener("click", (event) =>{
+        event.target.style.backgroundColor = currentColors.style.backgroundColor;
+    });
+};
