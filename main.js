72cf00585069999d0d953ef307c405b8ef69f868
james russell <jamesrussell@pursuit.org>
2021-09-24 18:10:52 -0400

    passed baseline tests

--- main.js
+++ main.js
@@ -7,3 +7,34 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+// creates an array like thing that you can select and iterate through
+let paletteColors = document.querySelectorAll(".color")
+let currentColor = document.querySelector("#current-color")
+
+// iterate through the colors in your palette and add a click even so that
+// when a palatte color is picked it changes current color to that color 
+for(let color of paletteColors) {
+  color.addEventListener("click", (e) => {
+    // sets current color to the color that was clicked on the palette
+     currentColor.style.backgroundColor = e.target.style.backgroundColor
+  })
+}
+
+// create an array like thing with your so you can add an
+// click event that changes the color of the cell to the current color
+let cells = document.querySelectorAll(".cell")
+
+// loop through cells and add click event
+for(let cell of cells) {
+  cell.addEventListener("click", (e) => {
+    // set target cell color to equal what current color chosen is
+    e.target.style.backgroundColor = currentColor.style.backgroundColor
+  })
+}
+
+// for(let cell of cells) {
+//   cell.addEventListener("click", () => {
+//     e.target.setAttribute("style", `background: {currentColor.style.background}`)
+//   })
+// }
