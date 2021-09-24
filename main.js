4d52eaf76dbfbe72fe9f47428c10b3ef49bceba2
rae charles <r_charles87@yahoo.com>
2021-09-24 15:38:08 -0400

    finished lab working on extras

--- main.js
+++ main.js
@@ -7,3 +7,31 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+// let currentColor = document.querySelector("#current-color");
+// let palettes = document.querySelectorAll("#palette .color")
+// for (let palette of palettes){
+//   let color = document.querySelector("palette.style.background")
+// palette.addEventListener("click", () => {
+//   currentColor.style.background= "color"
+// })
+// }
+
+// "palette.style.background"
+
+let palettes = document.querySelectorAll("#palette .color")
+for (let palette of palettes){
+palette.addEventListener("click", (event) => {
+    let currentColor = document.querySelector("#current-color");
+    currentColor.style.backgroundColor = event.target.style.backgroundColor
+     
+  })
+}
+
+let cells = document.querySelectorAll(".cell")
+for (let cell of cells){
+  cell.addEventListener("click", ()=>{
+    let color = document.querySelector("#current-color")
+    cell.style.backgroundColor = color.style.backgroundColor
+  })
+}
