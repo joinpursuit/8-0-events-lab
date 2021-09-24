69d77d70a69b301de5eb1dc822e820b6782e8811
Melissa Lopez <1zuzsuit492@Melissas-MacBook-Pro.local>
2021-09-24 15:44:19 -0400

    mucho colores.

--- main.js
+++ main.js
@@ -7,3 +7,25 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+const colores = document.querySelectorAll('.color');
+//got .color from css classes
+
+const selected = document.querySelector('#current-color');
+//selecting div id=current-color
+
+for (const color of colores){
+  color.addEventListener("click", () => {
+    selected.style.background = color.style.background;
+    //when clicked, the indicator becomes the same color as the chosen background color//
+  });
+}
+
+const cells = document.querySelectorAll('.cell');
+  for(const cell of cells){
+    cell.addEventListener("click", () => {
+      cell.style.background = selected.style.background;
+      //cell color now matches the selected color.//
+    });;
+  }
+
