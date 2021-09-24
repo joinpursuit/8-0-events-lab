af30670311d668717598143faf7638ca86380c83
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2021-09-24 13:10:21 -0400

    lab done

--- main.js
+++ main.js
@@ -7,3 +7,21 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+const colors = document.querySelectorAll('.color');
+const currentColor = document.querySelector('#current-color');
+
+colors.forEach((color) => {
+  color.addEventListener('click', () => {
+    currentColor.style.background = color.style.background;
+  })
+})
+
+
+
+const cells = document.querySelectorAll('.cell');
+
+cells.forEach((cell) => {
+  cell.addEventListener('click', () => {
+    cell.style.background = currentColor.style.background;
+  })
+})
