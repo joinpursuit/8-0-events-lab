f33282b69c47e3e58885cf9f0522c71cc300ed97
salrey <tekdehn@gmail.com>
2021-09-24 12:59:28 -0400

    Change the background of the current color selected

--- main.js
+++ main.js
@@ -7,3 +7,12 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+const colors = document.querySelectorAll(".color");
+
+const currentColor = document.querySelector("#current-color");
+
+colors.forEach((color) => {
+  color.addEventListener("click", () => {
+    currentColor.style.background = color.style.background;
+  });
+});
