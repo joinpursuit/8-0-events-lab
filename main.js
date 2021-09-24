a38de67ce0c91edb59db0dd9fe521475e5a9c232
salrey <tekdehn@gmail.com>
2021-09-24 15:55:52 -0400

    Add cells without affecting overall size of the #canvas

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Do not change the code below.
 const main = document.querySelector("main");
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 150; i++) {
   const div = document.createElement("div");
   div.classList.add("cell");
   main.append(div);
