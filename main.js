66f030e4832388f22bdc21c5035e6da2f92c98b2
Christina Loiacono <loiaconochristina@gmail.com>
2021-09-24 14:22:03 -0400

    background of current color changes when a color is clicked

--- main.js
+++ main.js
@@ -7,3 +7,41 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+// select all colors
+let colors = document.querySelectorAll("#palette .color");
+// console.log(colors);
+
+// select the current color
+let current = document.getElementById('current-color');
+current.setAttribute("style", "background: null");
+// console.log(current);
+
+for (let color of colors) {
+  color.addEventListener("click", () => {
+    console.log("this is your new color");
+    if (color === colors[0]){
+      current.setAttribute("style", "background: black");
+    }
+    if (color === colors[1]){
+      current.setAttribute("style", "background: red");
+    }
+    if (color === colors[2]){
+      current.setAttribute("style", "background: orange");
+    }
+    if (color === colors[3]){
+      current.setAttribute("style", "background: yellow");
+    }
+    if (color === colors[4]){
+      current.setAttribute("style", "background: green");
+    }
+    if (color === colors[5]){
+      current.setAttribute("style", "background: blue");
+    }
+    if (color === colors[6]){
+      current.setAttribute("style", "background: purple");
+    }
+    if (color === colors[7]){
+      current.setAttribute("style", "background: white");
+    }
+  })
+}
