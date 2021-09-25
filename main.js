79b7d6b75c7e02653d36b572543a7b7b7fb35882
theodorefrazier <theo.frazier@icloud.com>
2021-09-25 15:34:16 -0400

    added notes to break down what everything was doing, renamed color section eventListener to be less confusing,wrapped everything a function named colorSelection and called it - still working

--- main.js
+++ main.js
@@ -14,14 +14,16 @@ const currentColor = document.querySelector("#current-color")
 
 // Grab the seleted color in the palette which is in a CLASS of "color"
 
-const selectedColor = document.querySelectorAll(".color")  //remember to use QUERYSELECTORALL - and not just query selector, or it won't interate!!! 
+const availableColors = document.querySelectorAll(".color")  //remember to use QUERYSELECTORALL - and not just query selector, or it won't interate!!! 
 
 // Access the CLASS of cell 
 
 const accessCell = document.querySelector(".cell") 
 
-for (let color of selectedColor) {
+for (let color of availableColors) {
+  // created a event - and attached it each color of the available colors - this eventListener is listening out for a click action
   color.addEventListener("click", (event) => {
+    //cuurent colors styling and background color became equal to the even target's style and background color and style
     currentColor.style.backgroundColor = event.target.style.backgroundColor
   });
 }
