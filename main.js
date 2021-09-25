52a3b23ea431075fcba4b4f70ddcfddfd5b591be
Rohan-Sterling <rohansterling@pursuit.org>
2021-09-25 10:11:30 -0400

    add additional features

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Do not change the code below.
 const main = document.querySelector("main");
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 2500; i++) {
   const div = document.createElement("div");
   div.classList.add("cell");
   main.append(div);
@@ -8,21 +8,56 @@ for (let i = 0; i < 100; i++) {
 
 // You may write your code here!
 
-//query all div
-document.querySelectorAll('div').forEach(div => {
+const canvas = document.querySelector('#canvas')
+const currentColorViewer = document.querySelector('#current-color')
+const allCells = document.querySelectorAll('.cell')
 
-  //listen for click on any div
-  div.addEventListener('click', () => {
-    //declare variable `brush` = document.querySelector('#current-color')
-    const brush = document.querySelector('#current-color')
-
-    //Check if div is palette or canvas
-    //---->if palette, #current-color.style.background = div.style.background : div.style.background = #current-color.style.background
-    div.className === 'cell' ? div.style.background = brush.style.background : brush.style.background = div.style.background
-
+//query all divs
+document.querySelectorAll('#palette div').forEach(selectedColor =>{
+  selectedColor.addEventListener('click', () =>{
+    currentColorViewer.style.background = selectedColor.style.background
   })
-
 })
+
+
+const toggleActive = () => canvas.classList.toggle('active');
+
+
+//click and drag to draw
+allCells.forEach(cell =>{
+  cell.addEventListener('mousedown', toggleActive)
+  
+  cell.addEventListener('mouseup', toggleActive)
+
+  cell.addEventListener('mouseover', () => {
+    if(canvas.className === 'active'){
+    cell.style.background = currentColorViewer.style.background
+    }
+  })
+  cell.addEventListener('mousedown', () => {
+    cell.style.background = currentColorViewer.style.background
+  })
+})
+
+
+//RESET: double click current color div to reset the canvas
+currentColorViewer.addEventListener('dblclick', () => {
+  allCells.forEach(cell =>{
+    cell.style.background = 'white'
+    currentColorViewer.style.background = 'white'
+  })
+})
+
+//FILL: click current color div to fill canvas with selected color
+currentColorViewer.addEventListener('click', () => {
+  allCells.forEach(cell =>{
+    cell.style.background = currentColorViewer.style.background
+  })
+})
+
+
+
+
 
 
 
