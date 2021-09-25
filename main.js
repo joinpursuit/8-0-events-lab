a48c77b6def2c7bac72a155575bf15fee7e7b09b
Christina Loiacono <loiaconochristina@gmail.com>
2021-09-25 17:54:28 -0400

    added buttons with event listeners for clearing canvas, and making canvas all one color

--- main.js
+++ main.js
@@ -60,10 +60,49 @@ for (let color of colors) {
 // we want to play with all the `cell` tags, not just 1 `cell` tag
 let cells = document.querySelectorAll(".cell");
 for (let cell of cells) {
-  // what is the currently picked color?
-  let pickedColor = document.querySelector("#current-color");
-
+  // what is the currently picked color? `current`
   cell.addEventListener("click", (e)=>{
-  e.target.style.backgroundColor = pickedColor.style.backgroundColor;
+  e.target.style.backgroundColor = current.style.backgroundColor;
   })
 }
+
+/*
+MAKE BUTTON #1 - Clears all colors from canvas
+*/
+// create `clearButton` button
+let clearButton = document.createElement('div');
+// set attributes to element
+clearButton.setAttribute("class", "clear-button");
+clearButton.setAttribute("style", "text-align:center; color: white; background-color: #7e7f9a; border: 3px double white; display:block; margin-left: auto; margin-right: auto; margin-bottom:20px;");
+clearButton.textContent = "Clear The Canvas";
+
+// add to body
+main.before(clearButton);
+
+// add event listener for button - clears canvas
+clearButton.addEventListener("click", ()=>{
+  for (let cell of cells){
+    cell.style.backgroundColor = "";
+  }
+})
+
+
+/*
+MAKE BUTTON #2 - Makes canvas all Current Color
+*/
+// create `oneColor` button
+let oneColor = document.createElement('div');
+// set attributes to element
+oneColor.setAttribute("class", "one-button");
+oneColor.setAttribute("style", "text-align:center; color: white; background-color: #7e7f9a; border: 3px double white; display:block; margin-left: auto; margin-right: auto; margin-bottom:20px;");
+oneColor.textContent = "One Color Canvas";
+
+// add to body
+main.before(oneColor);
+
+// add event listener for button - makes canvas all current color
+oneColor.addEventListener("click", ()=>{
+  for (let cell of cells){
+    cell.style.backgroundColor = current.style.backgroundColor;
+  }
+})
