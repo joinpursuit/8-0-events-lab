6163924497504317b9b3bee52eb40f0c4311b314
Mukayila ALAO <mukayilaalao@pursuit.org>
2021-09-24 20:58:41 -0400

    removed duplicated codes and adding comments

--- main.js
+++ main.js
@@ -8,31 +8,35 @@ for (let i = 0; i < 100; i++) {
 
 // You may write your code here!
 
+
+// Grabbing the current color tag
 let currentColor=document.querySelector("#current-color");
+// Grabbing all cells using color class
 let allPalettes=document.querySelectorAll(".color");
+// Adding  event listener to each palette element and changing current element background color
 for(let div of allPalettes) {
   div.addEventListener("click", e => {
     currentColor.style.backgroundColor= e.target.style.backgroundColor;
   })
 }
+// Grabbing the last state of the current element background color
 let currentColor2=document.querySelector("#current-color");
-let cells=document.querySelectorAll(".cell");
-for(let cell of cells){
-  cell.addEventListener("click", e => {
-    e.target.style.backgroundColor=currentColor2.style.backgroundColor;
-  })
-}
+// Grabbing canvas element
 let canvas=document.getElementById("canvas");
-const main2 = document.querySelector("main");
+// Grabbing main element
+let main2 = document.querySelector("main");
+// Creating 9900 more div elements
 for (let i = 0; i < 9900; i++) {
   const div = document.createElement("div");
   div.classList.add("cell");
   main2.append(div);
 }
+// Grabbing all the cells
 let cells2=document.querySelectorAll(".cell");
+// Changing canvas columns to 100 and 5px
 canvas.style.gridTemplateColumns= "repeat(100, 5px)";
-canvas.style.gridTemplaterows= "5px";
-
+// canvas.style.gridTemplaterows= "5px";
+// Setting width and height of each cell to 5px and adding event listener to them
 for(let cell of cells2){
   cell.style.width="5px";
   cell.style.height="5px";
