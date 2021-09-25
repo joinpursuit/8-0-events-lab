57956f81274d7599ce129235a73135812fa2f282
Mukayila ALAO <mukayilaalao@pursuit.org>
2021-09-24 22:38:52 -0400

    Close to final state

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ for (let i = 0; i < 9900; i++) {
 let cells2=document.querySelectorAll(".cell");
 // Changing canvas columns to 100 and 5px
 canvas.style.gridTemplateColumns= "repeat(100, 5px)";
-// canvas.style.gridTemplaterows= "5px";
+
 // Setting width and height of each cell to 5px and adding event listener to them
 for(let cell of cells2){
   cell.style.width="5px";
@@ -45,18 +45,31 @@ for(let cell of cells2){
   });
 }
 // Creating reset button
-let button=document.createElement("button");
-button.setAttribute("name", "Reset");
-button.setAttribute("id", "reset-id");
-button.innerText="Reset here";
-button.setAttribute("style", "width: 100px; margin: 25px 500px; text-align: center; border-radius:10px;");
+let buttonArr=[{name:"reset"},{name:"same color"}];
+for(let i=0; i<2; i++){
+  let button=document.createElement("button");
+  button.setAttribute("name", buttonArr[i].name);
+  button.setAttribute("id", buttonArr[i].name+"-id");
+  button.innerText= buttonArr[i].name+" here";
+  button.setAttribute("style", "width: 100px; margin: 25px 500px; text-align: center; border-radius:10px; background-color:gray;");
+  // select body and add button element
+ let body=document.querySelector("body");
+ body.append(button);
+// adding event to button
+ button.addEventListener("click", ()=> {
+   if(i===0) {
+    location.reload();
+   }
+   else {
+    let currentColor3=document.querySelector("#current-color");
+    let canvas=document.getElementById("canvas");
+    canvas.style.backgroundColor=currentColor3.style.backgroundColor;
+   }
 
-// select body
-let body=document.querySelector("body");
-body.append(button);
-button.addEventListener("click", ()=> {
-  location.reload();
 })
+}
+
+
 
 
 
