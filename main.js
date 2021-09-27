ec6702cc691f1b535ac86fd6cb090e7f445ea5ce
Laiba Sajid <sajid.laiba378@gmail.com>
2021-09-26 21:41:33 -0400

    added semi colons

--- main.js
+++ main.js
@@ -10,37 +10,43 @@ for (let i = 0; i < 100; i++) {
 let currentColor = document.querySelector("#current-color");
 let colorPalette = document.querySelectorAll("#palette .color");
 
+
 for(let color of colorPalette){
+  let studioGhibliImage4 = document.createElement("img");
+  studioGhibliImage4.setAttribute("src", "https://www.vhv.rs/dpng/d/511-5115101_transparent-spirited-away-png-spirited-away-no-face.png");
+  studioGhibliImage4.setAttribute("alt", "Image of Haku");
+  studioGhibliImage4.setAttribute("width", "30");
+  
+  color.append(studioGhibliImage4);
+
   color.addEventListener("click", () => {
     currentColor.style.backgroundColor = color.style.backgroundColor;
   })
 }
 
-let canvas = document.querySelectorAll("#canvas .cell")
+let canvas = document.querySelectorAll("#canvas .cell");
 
 for (let cell of canvas){
-  let studioGhibliImage2 = document.createElement("img")
-  studioGhibliImage2.setAttribute("src", "https://www.pngkey.com/png/full/188-1887025_i-made-a-cute-and-quick-transparent-calcifer.png")
+  let studioGhibliImage2 = document.createElement("img");
+  studioGhibliImage2.setAttribute("src", "https://www.pngkey.com/png/full/188-1887025_i-made-a-cute-and-quick-transparent-calcifer.png");
   studioGhibliImage2.setAttribute("alt", "Image of Calcifur")
-  studioGhibliImage2.setAttribute("width", "35")
+  studioGhibliImage2.setAttribute("width", "35");
 
-  cell.setAttribute("style", "backgroundColor")
+  cell.setAttribute("style", "backgroundColor");
 
   cell.addEventListener("click", () =>{
-    console.log("hello you clicked on a cell")
-    cell.style.backgroundColor = currentColor.style.backgroundColor
-
-
+    console.log("hello you clicked on a cell");
+    cell.style.backgroundColor = currentColor.style.backgroundColor;
   })
 
-  studioGhibliImage3 = document.createElement("img")
-  studioGhibliImage3.setAttribute("src", "https://i.pinimg.com/originals/29/0a/70/290a70e82fcb16e9ce54b452e1604599.png")
-  studioGhibliImage3.setAttribute("alt", "Image of Totoro")
-  studioGhibliImage3.setAttribute("width", "48")
+  studioGhibliImage3 = document.createElement("img");
+  studioGhibliImage3.setAttribute("src", "https://i.pinimg.com/originals/29/0a/70/290a70e82fcb16e9ce54b452e1604599.png");
+  studioGhibliImage3.setAttribute("alt", "Image of Totoro");
+  studioGhibliImage3.setAttribute("width", "48");
 
   cell.addEventListener("mouseover", () => {
-    cell.append(studioGhibliImage2)
-    cell.append(studioGhibliImage3)
+    cell.append(studioGhibliImage2);
+    cell.append(studioGhibliImage3);
   })
 }
 //EXtra 
@@ -48,9 +54,9 @@ for (let cell of canvas){
 
 
 currentColor.addEventListener("mouseover", () =>{
-  let studioGhibliImage1 = document.createElement("img")
-  studioGhibliImage1.setAttribute("src", "./ponyo.png")
-  studioGhibliImage1.setAttribute("alt", "Image of Ponyo")
-  studioGhibliImage1.setAttribute("width", "15")
-  currentColor.append(studioGhibliImage1)
+  let studioGhibliImage1 = document.createElement("img");
+  studioGhibliImage1.setAttribute("src", "./ponyo.png");
+  studioGhibliImage1.setAttribute("alt", "Image of Ponyo");
+  studioGhibliImage1.setAttribute("width", "15");
+  currentColor.append(studioGhibliImage1);
 })
