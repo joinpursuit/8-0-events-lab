0fac671766473acdf2f305fe0673733a5ef94e2c
Laiba Sajid <sajid.laiba378@gmail.com>
2021-09-26 21:03:58 -0400

    added studio ghibli stickers

--- main.js
+++ main.js
@@ -19,11 +19,27 @@ for(let color of colorPalette){
 let canvas = document.querySelectorAll("#canvas .cell")
 
 for (let cell of canvas){
+  let studioGhibliImage2 = document.createElement("img")
+  studioGhibliImage2.setAttribute("src", "https://www.pngkey.com/png/full/188-1887025_i-made-a-cute-and-quick-transparent-calcifer.png")
+  studioGhibliImage2.setAttribute("alt", "Image of Calcifur")
+  studioGhibliImage2.setAttribute("width", "25")
+
   cell.setAttribute("style", "backgroundColor")
+
   cell.addEventListener("click", () =>{
     console.log("hello you clicked on a cell")
     cell.style.backgroundColor = currentColor.style.backgroundColor
+    cell.append(studioGhibliImage2)
   })
 }
+//EXtra 
+let studioGhibliImage1 = document.createElement("img")
+studioGhibliImage1.setAttribute("src", "./ponyo.png")
+studioGhibliImage1.setAttribute("alt", "Image of Ponyo")
+studioGhibliImage1.setAttribute("width", "15")
 
-//look over target
+
+
+currentColor.addEventListener("mouseover", () =>{
+  currentColor.append(studioGhibliImage2)
+})
