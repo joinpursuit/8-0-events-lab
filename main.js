530fa21e6ef8feb247ab78f1c7a74c729d933e7e
andres ballares <andr3sb@hotmail.com>
2021-09-26 23:38:47 -0400

    done

--- main.js
+++ main.js
@@ -7,3 +7,21 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+const allColor = document.querySelectorAll(".color");
+const selectedColor = document.querySelector("#current-color");
+
+for (const color of allColor) {
+  color.addEventListener("click", () => {
+    selectedColor.style.background = color.style.background;
+  });
+}
+
+
+const allCells = document.querySelectorAll(".cell");
+
+for (const cell of allCells) {
+  cell.addEventListener("click", () => {
+    cell.style.background = selectedColor.style.background;
+  })
+}
