0ea8d602eb564d9e3f4e3a356ce158d4bb907be8
Cristobal Arias <cristobal.arias.13@gmail.com>
2021-09-26 22:36:49 -0400

    Complete lab

--- main.js
+++ main.js
@@ -1,9 +1,54 @@
 // Do not change the code below.
 const main = document.querySelector("main");
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 2500; i++) {
   const div = document.createElement("div");
   div.classList.add("cell");
   main.append(div);
 }
 
 // You may write your code here!
+// We get #current color
+// We get colors from #palette
+// We change current color's color to that color
+
+// When a color from the palette gets clicked, change current-color's color to that color
+//
+
+// Declare a variable that holds a value of #current-color
+
+const paletteColor = document.querySelectorAll("#palette .color");
+
+let currentColor = document.querySelector("#current-color");
+
+paletteColor.forEach((color) => {
+  color.addEventListener("click", () => {
+    currentColor.style.background = color.style.background;
+  });
+});
+
+const cells = document.querySelectorAll(".cell");
+
+cells.forEach((cell) => {
+  cell.addEventListener("click", () => {
+    cell.style.background = currentColor.style.background;
+  });
+});
+
+const reset = document.createElement("div");
+reset.classList.add("clear");
+
+document.querySelector("#canvas").before(reset);
+
+reset.addEventListener("click", () => {
+  cells.forEach((cell) => {
+    cell.style.background = "white";
+  });
+});
+
+const fillIn = document.querySelector(".rainbow-button");
+
+fillIn.addEventListener("click", () => {
+  cells.forEach((cell) => {
+    cell.style.background = currentColor.style.background;
+  });
+});
