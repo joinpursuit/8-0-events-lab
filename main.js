d3fbec6301cb9cdb52b0146a24e6f94aa560525b
Fatema Kabir <fatema.kabir@pursuit.org>
2021-09-26 20:07:33 -0400

    incresaed-number-of-cells

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Do not change the code below.
 const main = document.querySelector("main");
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 2500; i++) {
   const div = document.createElement("div");
   div.classList.add("cell");
   main.append(div);
