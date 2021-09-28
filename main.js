20e73d8319c9534276dd86504ec76f59764853c0
Larisa <larisashapkinab@gmail.com>
2021-09-28 10:59:08 -0400

    8-0-events-lab-done

--- main.js
+++ main.js
@@ -7,3 +7,23 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+const currentColor = document.querySelector("#current-color");
+
+const paletteColors = document.querySelectorAll(".color");
+for(let palette of paletteColors){
+  palette.addEventListener("click", (event) => {
+    currentColor.style.backgroundColor = event.target.style.backgroundColor;
+  });
+};
+
+//event = paletteColors (it's grabbing all the colors in palette and we can give to event any name)
+
+const cells = document.querySelectorAll(".cell");
+for(let cell of cells){
+  cell.addEventListener("click", (event)=>{
+    event.target.style.backgroundColor = currentColor.style.backgroundColor;
+  })
+}
+//event = cell
+
