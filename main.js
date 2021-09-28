16429414fabe917cc34ff31c96bd77d429ea8126
Billy Dominique <billydominique@pursuit.org>
2021-09-28 10:12:56 -0400

    fixed grid error

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 // Do not change the code below.
 const main = document.querySelector("main");
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 196; i++) {
   const div = document.createElement("div");
   div.classList.add("cell");
   main.append(div);
 }
 
 // You may write your code here!
+
 const colorButton = document.querySelectorAll("div.color");
 const currentColor = document.querySelector("#current-color");
 
