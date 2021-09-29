09dde1adf40e56f26c7d28159e106f617ad72db6
“Lili <lilyhuang114classb@gmail.com>
2021-09-28 23:52:28 -0400

    updated.

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
 let divs = document.querySelectorAll(".color");
 let currentColor = document.querySelector("#current-color");
 for(let div of divs) {
@@ -18,7 +19,36 @@ for(let div of divs) {
 let cells = document.querySelectorAll(".cell");
 
 for(let cell of cells) {
+  cell.style.textAlign = "center";
   cell.addEventListener("click", (element)=>{
+    
     element.target.style.backgroundColor = currentColor.style.backgroundColor;
+    
+    switch (element.target.style.backgroundColor) {
+      case "black":
+        element.target.textContent = "🌛";
+        break;
+      case "red":
+        element.target.textContent = "🍎";
+        break;
+      case "orange":
+        element.target.textContent = "🍊";
+        break;
+      case "yellow":
+        element.target.textContent = "🍌";
+        break;
+      case "green":
+        element.target.textContent = "🥑";
+        break;
+      case "blue":
+        element.target.textContent = "🫐";
+        break;
+      case "purple":
+        element.target.textContent = "🍇";
+        break;
+      case "white":
+        element.target.textContent = "🥥";
+        break;
+    }
   })
 }
