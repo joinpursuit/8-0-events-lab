227b5f6cb9e7cbdee961953087ae3a367a64e750
HectorIlarraza <53978521+HectorIlarraza@users.noreply.github.com>
2021-09-28 23:54:57 -0400

    Completed Events lab Hec & Rae

--- main.js
+++ main.js
@@ -7,3 +7,20 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+// console.log(cc);
+
+let palettes = document.querySelectorAll("#palette .color");
+for(let palette of palettes){
+  palette.addEventListener("click", (e) => {
+    let currentColor = document.querySelector("#current-color");
+    currentColor.style.backgroundColor = e.target.style.backgroundColor; 
+  });
+}
+
+let cells = document.querySelectorAll(".cell");
+for(let cell of cells){
+  cell.addEventListener("click", () => {
+    let cc = document.querySelector("#current-color");
+    cell.style.backgroundColor = cc.style.backgroundColor;
+  });
+}
