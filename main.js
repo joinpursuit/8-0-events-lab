be9b29a81b2743cd1f2b995c4533e42be4f1beee
David Vidals <david.vidals08@gmail.com>
2021-09-28 22:30:31 -0400

    Final commit

--- main.js
+++ main.js
@@ -7,3 +7,25 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+
+const currentColor = document.querySelector("#current-color"); 
+
+const colors = document.querySelectorAll(".color");
+
+const cells = document.querySelectorAll(".cell");
+
+
+// when a color from the bottom is clicked , the color will now be used to paint the cells
+for(let color of colors){
+  color.addEventListener("click", (event) =>{
+    currentColor.style.backgroundColor = event.target.style.backgroundColor;
+});
+};
+
+// when a cell is clicked in the grid, the color will change based on the color selected
+for(let cell of cells){
+  cell.addEventListener("click", (event) =>{
+    event.target.style.backgroundColor = currentColor.style.backgroundColor;
+});
+};
