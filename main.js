5bef69372f300e2a49c8562c5f52a98b136f9079
Antonio Shivers <antonioshivers2500@yahoo.com>
2022-03-29 11:37:57 -0400

    completed lab

--- main.js
+++ main.js
@@ -7,3 +7,18 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+const palette = document.getElementsByClassName("color");
+const currentColor = document.getElementById("current-color");
+const cells = document.getElementsByClassName("cell");
+
+for (let color of palette) {
+  color.addEventListener("click", () => {
+    currentColor.style.backgroundColor = color.style.backgroundColor;
+  });
+}
+
+for (let cell of cells) {
+  cell.addEventListener("click", () => {
+    cell.style.backgroundColor = currentColor.style.backgroundColor;
+  });
+}
