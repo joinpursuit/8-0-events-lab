7d92f5fd4b7a07e5f5cd2c5cedce4c691f6fe113
JuHaoChen1997 <juhaochen97@gmail.com>
2022-03-29 14:49:31 -0400

    passed all tests

--- main.js
+++ main.js
@@ -7,3 +7,22 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+/* When a color is clicked in the #palette, the #current-color element should have its background changed to that color. For example, if you click the green circle in the #palette, the #current-color element's background should change to be green.*/
+let palettes = document.querySelectorAll(".color");
+palettes.forEach((palette) => {
+  palette.addEventListener("click", () => {
+    const currentColor = document.querySelector("#current-color");
+    currentColor.style.backgroundColor = palette.style.backgroundColor;
+  });
+});
+
+// When you click on a .cell, its background should change to match the background of #current-color.
+let cells = document.querySelectorAll(".cell");
+cells.forEach((cell) => {
+  cell.addEventListener("click", () => {
+    const color =
+      document.querySelector("#current-color").style.backgroundColor;
+    cell.style.backgroundColor = color;
+  });
+});
