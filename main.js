0880c141df70165f53800872fc267ca0fd2d2954
EmaleeSoto <emaleesoto@gmail.com>
2022-03-29 11:59:17 -0400

    Compelted Events Lab

--- main.js
+++ main.js
@@ -7,3 +7,17 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+let currentColor = document.getElementById("current-color");
+let colors = document.getElementsByClassName("color");
+for (let color of colors) {
+  color.addEventListener("click", (event) => {
+    currentColor.style.backgroundColor = event.target.style.backgroundColor;
+  });
+}
+
+let cells = document.getElementsByClassName("cell");
+for (let cell of cells) {
+  cell.addEventListener("click", (event) => {
+    event.target.style.backgroundColor = currentColor.style.backgroundColor;
+  });
+}
