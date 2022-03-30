8f9c8149b5b90e8f63c4fb3492a1cc9a36087ddc
Antonio Shivers <antonioshivers2500@yahoo.com>
2022-03-30 09:10:24 -0400

    added a reset and fill button

--- main.js
+++ main.js
@@ -18,7 +18,71 @@ for (let color of palette) {
 }
 
 for (let cell of cells) {
-  cell.addEventListener("click", () => {
+  cell.addEventListener("mouseover", () => {
     cell.style.backgroundColor = currentColor.style.backgroundColor;
   });
 }
+
+const resetButton = document.createElement("button");
+resetButton.textContent = "RESET";
+resetButton.style.width = "10%";
+resetButton.style.height = "50px";
+resetButton.style.backgroundColor = "black";
+resetButton.style.color = "red";
+resetButton.style.marginRight = "35%";
+document.body.append(resetButton);
+
+resetButton.addEventListener("click", () => {
+  for (let cell of cells) {
+    cell.style.backgroundColor = "white";
+  }
+});
+
+const fillCells = document.createElement("button");
+fillCells.textContent = "FILL";
+fillCells.style.width = "10%";
+fillCells.style.height = "50px";
+fillCells.style.backgroundColor = "orange";
+fillCells.style.color = "black";
+fillCells.style.marginRight = "35%";
+document.body.append(fillCells);
+
+fillCells.addEventListener("click", () => {
+  for (let cell of cells) {
+    cell.style.backgroundColor = currentColor.style.backgroundColor;
+  }
+});
+
+//
+
+// const switchMouseMode = document.createElement("button");
+// switchMouseMode.textContent = "CLICKING";
+// switchMouseMode.style.width = "10%";
+// switchMouseMode.style.height = "50px";
+// switchMouseMode.style.backgroundColor = "lightblue";
+// switchMouseMode.style.color = "red";
+// document.body.append(switchMouseMode);
+
+// switchMouseMode.addEventListener("click", () => {
+//   if (switchMouseMode.textContent === "CLICKING") {
+//     switchMouseMode.textContent = "HOVERING";
+//     for (let cell of cells) {
+//       cell.addEventListener("mouseover", () => {
+//         cell.style.backgroundColor = currentColor.style.backgroundColor;
+//       });
+//     }
+//   } else {
+//     switchMouseMode.textContent = "CLICKING";
+//     for (let cell of cells) {
+//       cell.addEventListener("click", () => {
+//         cell.style.backgroundColor = currentColor.style.backgroundColor;
+//       });
+//     }
+//   }
+// });
+
+// function switchingModes() {
+
+// }
+
+// switchingModes();
