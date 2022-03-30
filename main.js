4735030b2abf72b40ec731a8a222ac03d5275b11
Andres Varon <anvaron@gmail.com>
2022-03-30 14:59:29 -0400

    DOM-Events lab - Final release

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ for (let i = 0; i < 100; i++) {
   div.classList.add("cell");
   main.append(div);
 }
-
+document.getElementById
 // You may write your code here!
 const colors = document.querySelectorAll('#palette .color')
 const current = document.querySelector('#current-color');
@@ -24,24 +24,27 @@ for(let color of colors){
 }
 
 const btnAdd   = document.createElement("button");
+const btnRem   = document.createElement("button");
 const btnReset = document.createElement("button");
 const btnFill  = document.createElement("button");
 btnAdd.textContent   = 'Add Cells';
+btnRem.textContent   = 'Remove Cells';
 btnReset.textContent = 'Reset Cells';
 btnFill.textContent  = 'Fill Cells';
-actions.append(btnAdd, btnReset, btnFill);
-//
+actions.append(btnAdd, btnRem, btnReset, btnFill);
+// Buttom events
 btnAdd.addEventListener('click', () => {
-  const div = document.createElement("div");
-  div.classList.add("cell");
-  main.appendChild(div)
-  console.log(main.children)
-  // let increment = 100;
-  // for (let i = 0; i < increment++; i++) {
+  // cells.forEach( cell => {cell.remove();})
+  // for (let i = 0; i < 100+1; i++) {
   //   const div = document.createElement("div");
-  //   div.classList.add("cell");
+  //   div.classList.add('cell');
+  //   div.setAttribute(`style`, `width: auto;height: auto`)
   //   main.append(div);
-  // }
+  //   console.log(cells)
+  //}
+})
+btnRem.addEventListener('click', () => {
+  main.removeChild(document.querySelector(".cell"));
 })
 btnReset.addEventListener('click', () => {
   for(let cell of cells){
@@ -49,6 +52,7 @@ btnReset.addEventListener('click', () => {
   }
 })
 btnFill.addEventListener('click', () => {
+  console.log(cells)
   for(let cell of cells){
     cell.setAttribute(`style`, `background: ${current.style.backgroundColor}`)
   }
