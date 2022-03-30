4390ab526c6b3d9cc2c0e904d22c1f16616ac7de
Derek <plutus.derekpeterson@gmail.com>
2022-03-29 21:53:45 -0400

    Event Lab Completed

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 // Do not change the code below.
-const main = document.querySelector("main");
+
+const main = document.querySelector('main');
 for (let i = 0; i < 100; i++) {
-  const div = document.createElement("div");
-  div.classList.add("cell");
+  const div = document.createElement('div');
+  div.classList.add('cell');
   main.append(div);
 }
-
 // You may write your code here!
+
+
+//Passed all lab tests:
+let currentColor = document.getElementById('current-color');
+let colors = document.getElementsByClassName('color');
+let cells = document.getElementsByClassName('cell');
+
+for (let color of colors) {
+  color.addEventListener('click', (event) => {
+    currentColor.style.background = event.target.style.background;
+  });
+}
+
+for (let cell of cells) {
+  cell.addEventListener('click', (event) => {
+    event.target.style.background = currentColor.style.background;
+  });
+}
+
