de003297eac06fe6fc0c25bd03900e841fd149e6
bowersjuan <bowersjuan@gmail.com>
2022-03-30 01:36:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,3 +7,42 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+const colors = document.querySelectorAll(".color");
+const currentColor = document.querySelector("#current-color");
+for (let color of colors) {
+  color.addEventListener("click", () => {
+    currentColor.setAttribute(
+      "style",
+      `background-color: ${color.style.backgroundColor};`
+    );
+  });
+} //
+
+const cells = document.querySelectorAll(".cell");
+for (let cell of cells) {
+  cell.addEventListener("click", () => {
+    cell.setAttribute(
+      "style",
+      `background-color: ${currentColor.style.backgroundColor};`
+    );
+  });
+}
+
+const clearButton = document.createElement("button");
+clearButton.textContent = "Clear Canvas";
+const body = document.querySelector("body");
+body.append(clearButton);
+clearButton.addEventListener("click", () => {
+  for (let cell of cells) {
+    cell.style.backgroundColor = "white";
+  }
+});
+
+const fillCanvasButton = document.createElement("button");
+fillCanvasButton.textContent = "Fill Canvas";
+currentColor.after(fillCanvasButton);
+fillCanvasButton.addEventListener("click", () => {
+  for (let cell of cells) {
+    cell.style.backgroundColor = `${currentColor.style.backgroundColor}`;
+  }
+});
