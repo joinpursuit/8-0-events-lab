c26f0b05df620077b424c187e1f834ad965f0733
Andres Varon <anvaron@gmail.com>
2022-03-30 09:04:40 -0400

    test-01: template tested

--- main.js
+++ main.js
@@ -7,3 +7,17 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+const colors = document.querySelectorAll('#palette .color')
+const current = document.querySelector('#current-color');
+current.setAttribute(`style`, `background: #FFF`)
+// const cells = document.querySelectorAll('#canvas .cell')
+// for(let cell of cells){
+//   cell.addEventListener('click', () => {
+//     cell.setAttribute(`style`, `background: ${current.style.backgroundColor}`)
+//   })
+// }
+for(let color of colors){
+  color.addEventListener('click', () => {
+    current.setAttribute(`style`, `background: ${color.style.backgroundColor}`)
+  })
+}
