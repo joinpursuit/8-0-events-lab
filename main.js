ad80b29be72232256663dbc91b457653dffb87c7
arupay <arupay11374@gmail.com>
2022-03-30 08:53:13 -0400

    added event listener loops for color pallete and cells

--- main.js
+++ main.js
@@ -7,3 +7,21 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+const currentColor = document.getElementById("current-color");
+const colorPalette = document.getElementsByClassName("color");
+console.log(colorPalette);
+
+for (let color of colorPalette) {
+  color.addEventListener("click", () => {
+    currentColor.style.background = color.style.background;
+  });
+}
+
+const cells = document.getElementsByClassName("cell");
+
+for (let cell of cells) {
+  cell.addEventListener("click", () => {
+    cell.style.background = currentColor.style.background;
+  });
+}
