948692d1b56e5b7524bcc85b1871028436175ce9
Samantha Figueroa <samanthafigueroa@pursuit.org>
2022-03-30 21:40:09 -0400

    lab done

--- main.js
+++ main.js
@@ -7,3 +7,24 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+const currentColor = document.querySelector("#current-color");
+
+const colors = document.querySelectorAll(".color");
+
+colors.forEach(color => {
+  color.addEventListener("click", (event) => {
+    const background = event.target.style.backgroundColor;
+    // console.log(event.target.style)
+    currentColor.setAttribute("style", `background: ${background}`)
+  });
+});
+
+const cells = document.querySelectorAll(".cell");
+
+cells.forEach(cell => {
+  cell.addEventListener("click", () => {
+    cell.setAttribute("style", `background: ${currentColor.style.background}` )
+  })
+})
+
