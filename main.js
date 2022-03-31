493302f8b8f57fb969958ad668fea029e7c7a689
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-03-31 00:41:40 -0400

    "psuedo"

--- main.js
+++ main.js
@@ -7,26 +7,36 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+// create a variable currentColor and give it value of - use the document.querySelector to pick the id element current-color
 const currentColor = document.querySelector("#current-color");
 
+// create a variable palette and give it value of - use the document.querySelector to pick ALL the id element palette .color nodes from the nodelist 
 const palette = document.querySelectorAll("#palette .color");
 
+// iterate through the nodelist of colors
 palette.forEach(color => {
+
+  // when a color is "clicked" something EVENT will happen
   color.addEventListener("click", (event) =>{
     
+    // the current color style background will change to the color styl background selected with the "click"
     currentColor.style.background = color.style.background
 
   });
 })
 
+
+// create a variable divCell and give it value of - use the document.querySelector to pick ALL the class element cell nodes from the nodelist 
 const divCell = document.querySelectorAll(".cell");
 
-// const palette = document.querySelectorAll("#palette .color");
-
+//  iterate through the nodelist of cells
 divCell.forEach(cell => {
+
+  // when a cell is "clicked" something EVENT will happen
   cell.addEventListener("click", (event) =>{
 
-    
+    // the cell color style background will change to the current color style background selected with the "click"
     cell.style.backgroundColor = currentColor.style.background
 
   });
