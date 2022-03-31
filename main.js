05a3082ac67bc8291739f9a5d1c38c445a343e2e
Jahvon <jahvondevine-jones41@pursuit.org>
2022-03-30 21:07:52 -0400

    Submitting Events Lab

--- main.js
+++ main.js
@@ -7,3 +7,18 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+document.addEventListener("click", ({target}) => {
+  console.log(target);
+  if (target.className === 'color'){
+    document.getElementById("current-color").style.background = target.style.background
+  } else if (target.className === 'cell'){
+    console.log("it worked!")
+    target.style.background = document.getElementById('current-color').style.background;
+  } else if (target.id === 'theButton'){
+    const cells = document.getElementsByClassName('cell');
+    for (cell of cells) {
+      cell.style.background = 'white';
+    }
+    document.getElementById("current-color").style.background = "white"
+  }
+});
