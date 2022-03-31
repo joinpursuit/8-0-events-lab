12a63efe4154ad60251be24f65042d276725d371
Jorge Carrera <JCarrera994@gmail.com>
2022-03-30 22:20:38 -0400

    Events Lab

--- main.js
+++ main.js
@@ -7,3 +7,34 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+const colors = document.querySelectorAll('.color')
+
+const currentColor = document.querySelector('#current-color')
+
+for (let color of colors){
+  color.addEventListener('click' , () => {
+    currentColor.style.backgroundColor = color.style.backgroundColor
+  });
+}
+
+const divCell = document.querySelectorAll('.cell')
+
+for (let cell of divCell) {
+  cell.addEventListener('click' , () => {
+    cell.style.backgroundColor = currentColor.style.backgroundColor
+  });
+}
+
+const button = document.createElement('button');
+button.setAttribute("style", "padding: 15px; width: 70px; height: 10px; margin: 25px")
+button.textContent = "RESET";
+
+
+
+const section = document.querySelectorAll('section')
+section[section.length-1].after(button)
+
+button.addEventListener('click' , () => {
+  divCell.forEach(x => x.style.backgroundColor = 'white')
+})
