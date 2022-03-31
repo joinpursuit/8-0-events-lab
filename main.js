1357a423c4a7c71a777412a94d18278d8079855b
fakanbiomowunmi@gmail.com <fakanbiomowunmi@gmail.com>
2022-03-31 00:13:51 -0400

    Event Lab

--- main.js
+++ main.js
@@ -7,3 +7,20 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+const colors = document.querySelectorAll(".color");
+colors.forEach(color => {
+  color.addEventListener("click" , (e) => {
+const currentColor = document.querySelector("#current-color");
+const backgroundColor = color.style.background;
+currentColor.setAttribute("style", `background: ${backgroundColor}`)
+  })
+});
+
+const cells = document.querySelectorAll(".cell");
+cells.forEach(cell =>  {
+  cell.addEventListener("click" , (e) => {
+    const currentColor2 = document.querySelector ("#current-color");
+    const backgroundColor2 = currentColor2.style.background;
+    cell.setAttribute("style", `background: ${backgroundColor2}`)
+  })
+});
