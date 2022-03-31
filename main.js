6241158978a534d452fcd6829a40a3d52de088e2
Mendoza Holger <mendozadavidh111@gmail.com>
2022-03-30 20:40:00 -0400

    lab completed

--- main.js
+++ main.js
@@ -3,7 +3,23 @@ const main = document.querySelector("main");
 for (let i = 0; i < 100; i++) {
   const div = document.createElement("div");
   div.classList.add("cell");
-  main.append(div);
+  main.append(div); 
 }
 
 // You may write your code here!
+const currentColor = document.querySelector('#current-color');
+const palettes = document.querySelectorAll('#palette .color')
+
+for (let palette of palettes) {
+  palette.addEventListener('click', (e) => {
+    currentColor.style.backgroundColor = e.target.style.backgroundColor
+  })
+}
+
+
+const cells = document.getElementsByClassName('cell')
+for (let cell of cells) {
+  cell.addEventListener('click', (e) => {
+    e.target.style.backgroundColor = currentColor.style.backgroundColor;
+  })
+}
