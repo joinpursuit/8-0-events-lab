c1cfb2362aeb407541f3ce2e2001335a60cee406
Zane <zaneahmed4@gmail.com>
2022-03-30 21:03:25 -0400

    done

--- main.js
+++ main.js
@@ -7,3 +7,35 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+const currentColor = document.querySelector('#current-color')
+
+const colors = document.querySelectorAll('.color')
+colors.forEach(color => {
+  color.addEventListener('click', (event) => {
+    currentColor.style.background = color.style.background
+  })
+})
+
+// const colors = document.querySelectorAll('.color')
+// colors.forEach(color => {
+//   color.addEventListener('click', (event) => {
+//     const currentColor = document.querySelector('#current-color')
+//     currentColor.style.backgroundColor = color.style.backgroundColor
+//   })
+// })
+
+const cells = document.querySelectorAll('.cell')
+cells.forEach(cell => {
+  cell.addEventListener('click', (event) => {
+    cell.style.background = currentColor.style.background
+  })
+})
+
+// const cells = document.querySelectorAll('.cell')
+// cells.forEach(cell => {
+//   cell.addEventListener('click', (event) => {
+//     const currentColor = document.querySelector('#current-color')
+//     cell.style.backgroundColor += currentColor.style.backgroundColor
+//   })
+// })
