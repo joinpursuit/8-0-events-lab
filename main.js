364da1c64c625c1ea087156d282bb285a1f33713
Jyoti Singh <jyo.singh88@gmail.com>
2022-03-30 20:20:39 -0400

    Events

--- main.js
+++ main.js
@@ -7,3 +7,18 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+let currentColor = document.getElementById('current-color');
+
+
+let colors = document.getElementsByClassName('color');
+for (let color of colors) {
+  color.addEventListener('click', (event) => {
+    currentColor.style.background = event.target.style.background;
+  });
+}
+let cells = document.getElementsByClassName('cell');
+for (let cell of cells) {
+  cell.addEventListener('mousedown', (event) => {
+    event.target.style.background = currentColor.style.background;
+  });
+}
