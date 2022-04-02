7552590a6a2a058de8c72c9b8fe62aa7946c3750
Dannell <dannellmorales@pursuit.org>
2022-04-02 17:01:46 -0400

    Events

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Do not change the code below.
 const main = document.querySelector("main");
 for (let i = 0; i < 100; i++) {
   const div = document.createElement("div");
@@ -6,4 +5,20 @@ for (let i = 0; i < 100; i++) {
   main.append(div);
 }
 
-// You may write your code here!
+const colors = document.querySelectorAll(".color");
+colors.forEach(color => {
+  color.addEventListener("click", (e) => {
+    const currentColor = document.querySelector("#current-color");
+    const backgroundColor = color.style.background;
+    currentColor.setAttribute("style", `background: ${backgroundColor}`);
+  })
+})
+
+const cells = document.querySelectorAll(".cell");
+cells.forEach(cell => {
+  cell.addEventListener("click", (e) => {
+    const currentColor = document.querySelector("#current-color");
+    const backgroundColor = currentColor.style.background;
+    cell.setAttribute("style", `background: ${backgroundColor}`)
+  })
+})
