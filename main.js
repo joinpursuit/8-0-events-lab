9527958565d55c99fd59dafe1041e9eec1ba16db
Wilton <your_email_here>
2022-04-20 20:53:50 -0400

    Issues pushing

--- main.js
+++ main.js
@@ -5,5 +5,24 @@ for (let i = 0; i < 100; i++) {
   div.classList.add("cell");
   main.append(div);
 }
-
 // You may write your code here!
+const currentColor = document.querySelector("#current-color");
+
+const palette = document.querySelectorAll(".color");
+
+palette.forEach(color => {
+  color.addEventListener("click", (event) => {
+   
+  currentColor.setAttribute("style", `background: ${color.style.background}`);
+  })
+})
+
+const cell = document.querySelectorAll(".cell");
+
+cell.forEach(cell => {
+  cell.addEventListener("click", (event) => {
+    cell.setAttribute("style", `background: ${currentColor.style.background}`)
+
+  })
+
+})
